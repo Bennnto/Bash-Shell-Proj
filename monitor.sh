096496627b0397ad91e4c9a06d8aad0e50ba0a55
Bennnto <ben.promkaew@icloud.com>
2025-07-26 20:34:55 -0400

    intitial commit

---
+++ monitor.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "------------ System Report -------------"
+echo "📆 | $(date)"
+
+# CPU USAGE
+cpu_usage=$(top -l 1 | grep "CPU usage" | awk '{print $3}' | sed 's/%//')
+echo "🖥️ CPU | $cpu_usage%"
+
+# RAM USAGE
+ram_usage=$(vm_stat | awk '
+  /Pages active/     {active=$3}
+  /Pages inactive/   {inactive=$3}
+  /Pages speculative/{spec=$3}
+  /Pages wired down/ {wired=$3}
+  /Pages free/       {free=$3}
+  END {
+    used=(active+inactive+spec+wired)*4096;
+    total=(active+inactive+spec+wired+free)*4096;
+    printf "%.2f", used/total*100
+  }
+')
+echo "💾 RAM | $ram_usage%"
+
+# DISK USAGE
+echo "🗄️ DISK |"
+df -h /
+
+# BATTERY
+battery_percent=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
+echo "🔋 BAT | $battery_percent%"
